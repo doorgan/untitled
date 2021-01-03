60683bc48caf0e498ca6c6eb87437705ceae4205
doorgan <dorgandash@gmail.com>
2021-01-03 18:45:09 -0300

    Update stream typings

--- src/state.ts
+++ src/state.ts
@@ -6,7 +6,13 @@ const { stream, scan } = flyd;
 export type FunctionPatch<T> = (state: T) => T;
 export type Patch<T> = FunctionPatch<T> | Partial<T>;
 
-export type Stream<T> = flyd.Stream<T>;
+export interface Stream<T> {
+  (): T;
+  (value: T): Stream<T>;
+  map<V>(project: (value: T) => V): Stream<V>;
+  end: Stream<boolean>;
+  val: T;
+}
 
 export type Store<T> = Stream<T> & {
   update: Stream<Patch<T>>;
--- types/state.d.ts
+++ types/state.d.ts
@@ -1,6 +1,12 @@
 export declare type FunctionPatch<T> = (state: T) => T;
 export declare type Patch<T> = FunctionPatch<T> | Partial<T>;
-export declare type Stream<T> = flyd.Stream<T>;
+export interface Stream<T> {
+    (): T;
+    (value: T): Stream<T>;
+    map<V>(project: (value: T) => V): Stream<V>;
+    end: Stream<boolean>;
+    val: T;
+}
 export declare type Store<T> = Stream<T> & {
     update: Stream<Patch<T>>;
 };
